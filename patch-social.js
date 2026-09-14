5254eab993c85bfaaffbeaf623ec3cb925bb9974
Abdurahmon <hallaymstore@gmail.com>
2026-09-14 10:48:00 +0500

    Make social build patch idempotent

--- patch-social.js
+++ patch-social.js
@@ -5,13 +5,16 @@ const serverPath=path.join(root,'server.js');
 const indexPath=path.join(root,'public','index.html');
 
 let s=fs.readFileSync(serverPath,'utf8');
-if(!s.includes('LUMO_SOCIAL_V3')){
+const mounts=[];
+if(!s.includes("require('./social-runtime')({app,io,auth});"))mounts.push("require('./social-runtime')({app,io,auth});");
+if(!s.includes("require('./chat-admin-runtime')({app,io,auth});"))mounts.push("require('./chat-admin-runtime')({app,io,auth});");
+if(mounts.length){
   const needle='mongoose.connect(';
   const at=s.indexOf(needle);
   if(at<0)throw new Error('Lumo social patch: mongoose.connect target not found');
-  s=s.slice(0,at)+"/* LUMO_SOCIAL_V3 */\nrequire('./social-runtime')({app,io,auth});\nrequire('./chat-admin-runtime')({app,io,auth});\n\n"+s.slice(at);
+  s=s.slice(0,at)+"/* LUMO_SOCIAL_V3 */\n"+mounts.join('\n')+"\n\n"+s.slice(at);
   fs.writeFileSync(serverPath,s);
-  console.log('Lumo social backend v3 mounted');
+  console.log('Lumo social backend v3 mounted:',mounts.length);
 }
 
 let h=fs.readFileSync(indexPath,'utf8');
